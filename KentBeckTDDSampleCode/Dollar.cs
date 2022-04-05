5a12386836204a61bb8ef934e17f9943e3b22c81
Jacky <twjacky@hotmail.com>
2022-04-06 01:20:21 +0800

    [CH1] Red light

---
+++ KentBeckTDDSampleCode/Dollar.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace KentBeckTDDSampleCode
+{
+    public class Dollar
+    {
+        public int Amount;
+
+        public Dollar(int amount)
+        {
+
+        }
+
+        public void Times(int multiple)
+        {
+
+        }
+    }
+}
